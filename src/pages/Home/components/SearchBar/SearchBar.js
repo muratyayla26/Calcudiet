0b678ccd3c098176c2cd8fb1e46706d62b4c7855
hasanelmaci <mhasanelmaci@gmail.com>
2021-01-16 00:03:36 +0300

    format

---
+++ src/pages/Home/components/SearchBar/SearchBar.js
@@ -0,0 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {useState}from 'react'
+import styles from './_searchBar.module.scss'
+
+function SearchBar({fetch}) {
+
+    const [inputValue, setInputValue] = useState('')
+
+
+    return (
+        <div className={styles.searchBar}>
+            <form className={styles.searchForm}onSubmit={(e)=>fetch(inputValue,e)}>
+                <input type='text' className={styles.searchInput}value={inputValue} onChange={(e)=>setInputValue(e.target.value)} placeholder="Search"/>
+                <span type='submit'  className={styles.barBtn}><FontAwesomeIcon icon={faSearch} /></span>
+            </form>
+        </div>
+    )
+}
+
+export default SearchBar
