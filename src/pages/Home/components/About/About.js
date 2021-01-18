0da509fee3c3e07892efcfea676d4eb3ef45bb43
hasanelmaci <mhasanelmaci@gmail.com>
2021-01-18 20:41:18 +0300

    about section

---
+++ src/pages/Home/components/About/About.js
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import foods from "../../img/foods.png";
+import styles from "./_about.module.scss";
+
+function About() {
+  useEffect(() => {
+    function animations() {
+      document.addEventListener("scroll", function (e) {
+        var top = window.pageYOffset + window.innerHeight,
+          isVisible = top > document.querySelector(".home-about-head").offsetTop;
+
+        if (isVisible) {
+          document.querySelector(".home-about-head").classList.add("animate-about");
+          document.querySelector(".home-about-paragraph").classList.add("animate-about");
+        }
+      });
+    }
+
+    animations();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={`${styles.aboutContainer}`}>
+        <img src={foods} />
+
+        <h2 className={`${styles.homehead} home-about-head` }>
+          About Us
+        </h2>
+        <p className='home-about-paragraph'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
