0d65a13b13fbe21d411deabbdf64510898b36abf
hasanelmaci <mhasanelmaci@gmail.com>
2021-01-24 01:51:21 +0300

    added offer table

---
+++ src/pages/Home/components/Offer/FetchOffers.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "./_offer.module.scss";
+import Offer from "./Offer";
+
+function FetchOffers() {
+  const [select, setSelect] = useState("dessert");
+  const [offers, setOffers] = useState([]);
+
+  useEffect(() => {
+    const key = process.env.REACT_APP_KEY;
+    const appid = process.env.REACT_APP_APPID;
+    axios
+      .get(
+        `https://api.edamam.com/search?q=${select}&app_id=${appid}&app_key=${key}`
+      )
+      .then((res) => setOffers(res.data.hits));
+    console.log("ASD");
+  }, [select, setSelect]);
+
+  return (
+    <div>
+      <Offer setSelect={setSelect} offers={offers} />
+    </div>
+  );
+}
+
+export default FetchOffers;
