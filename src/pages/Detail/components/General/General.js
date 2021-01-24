18e2aafc3b46f898db7c80cb18069b3fe17c1a88
murat26 <74151892+muratyayla26@users.noreply.github.com>
2021-01-24 23:21:52 +0300

    verinin firestore eklenip eklenmedigini gösteren fonksiyon islemleri silindi. firestoredan veri silme fonksiyonu yazildiktan sonra tekrar tum ozellikler ile birlikte eklenecek

--- src/pages/Detail/components/General/General.js
+++ src/pages/Detail/components/General/General.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { addToStore } from "../../../../utility/addToStore";
 import styles from "./general.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -9,26 +8,12 @@ import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
 import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
 import { faYoutubeSquare } from "@fortawesome/free-brands-svg-icons";
 import { faWhatsappSquare } from "@fortawesome/free-brands-svg-icons";
-import { faCheckSquare } from "@fortawesome/free-regular-svg-icons";
-import { alreadyAddedChecker } from "../../../../utility/alreadyAddedChecker";
 
 const General = ({ recipe }) => {
-  const [added, setAdded] = useState(false);
-
-  useEffect(() => {
-    alreadyAddedChecker(recipe).then((response) => {
-      setAdded(response);
-    });
-  }, []);
-
   const clickHandler = () => {
     addToStore(recipe);
-    alreadyAddedChecker(recipe).then((response) => {
-      setAdded(response);
-    });
   };
 
-  console.log(added);
   return (
     <div className={styles.container}>
       <div className={styles["img-container"]}>
@@ -37,21 +22,10 @@ const General = ({ recipe }) => {
       <div className={styles["text-container"]}>
         <p className={styles.name}>{recipe.label}</p>
         <div className={styles.footer}>
-          {added ? (
-            <div className={styles["add-list"]}>
-              <span>Already In List</span>
-              <FontAwesomeIcon
-                className={styles["faIcon"]}
-                icon={faCheckSquare}
-              />
-            </div>
-          ) : (
-            <div onClick={clickHandler} className={styles["add-list"]}>
-              <span>Add To Your</span>
-              <FontAwesomeIcon className={styles["faIcon"]} icon={faListUl} />
-            </div>
-          )}
-
+          <div onClick={clickHandler} className={styles["add-list"]}>
+            <span>Add To Your</span>
+            <FontAwesomeIcon className={styles["faIcon"]} icon={faListUl} />
+          </div>
           <a
             href={recipe.url}
             rel="noreferrer"
