710c75391c0e8ecf3eeb173f7da727c644362a23
Ahmet <ahmetsehacar@gmail.com>
2021-02-02 16:16:22 +0300

    firestore veri ekleme fonksiyonu butona bağlandı

--- src/pages/Search/components/FoodCard/FoodCard.js
+++ src/pages/Search/components/FoodCard/FoodCard.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, {useContext} from "react";
 import styles from "./FoodCard.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUtensils } from "@fortawesome/free-solid-svg-icons";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../../utility/AuthContext"
+import { addToStore} from  "../../../../utility/addToStore"
+
 
 function FoodCard({ recipe }) {
+
+  const { currentUser } = useContext(AuthContext);
+  const userId = currentUser ? currentUser.uid : localStorage.getItem("userId");
+  const addToList = () => {
+    if (userId) {
+      addToStore(recipe, userId).then(response => {
+        if (response) {
+          console.log("veri store eklendi")
+        }else {
+          console.log("veri eklenmedi")
+        }
+      }) 
+    }else {
+      console.log("giriş yapılmadığı için veri eklenmedi")
+    }
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.FoodCard}>
@@ -23,7 +43,7 @@ function FoodCard({ recipe }) {
             <span> Calories</span>
           </p>
           <div className={styles.icon}>
-            <FontAwesomeIcon className={styles["Heart"]} icon={faHeart} />{" "}
+            <FontAwesomeIcon onClick={addToList} className={styles["Heart"]} icon={faHeart} />
             <Link to={`/detail/${recipe.uri.split("_")[1]}`}>
               <FontAwesomeIcon
                 className={styles["Utensils"]}
