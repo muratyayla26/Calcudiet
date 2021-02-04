7b817f02bf6bccee87aa3b7b55b3aebc7f010cc3
murat26 <74151892+muratyayla26@users.noreply.github.com>
2021-02-04 19:58:33 +0300

    yorum satiri

--- src/utility/deleteFromStore.js
+++ src/utility/deleteFromStore.js
@@ -1,7 +1,7 @@
 //veriyi firestoredan silmek icin kullanilan fonksiyon
 //ilk parametre yemegin id si, ikinci parametre userid si
 //fonksiyon true donerse veri basari ile silindi
-//fonksiyon false donerse firestore ile ilgili baglanti hatasi
+//fonksiyon false donerse firestore ile ilgili baglanti hatasi anlamina geliyor
 import { db } from "./firestore";
 
 export const deleteFromStore = async (recipeId, ownerId) => {
